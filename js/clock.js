1312fff7327420a717c9ce2aa166554a2903d3b8
Mert Şakar <mertsakar@hotmail.com>
2021-12-16 20:40:32 +0300

    Tek haneli zaman alanları icin basina sifir eklendi

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,16 @@
 function showTime(){
     let time = new Date();
     let hours = time.getHours();
+    hours < 10 ? (hours = `0${hours}`) : hours;
     let minutes = time.getMinutes();
+    minutes < 10 ? (minutes = `0${minutes}`) : minutes;
     let seconds = time.getSeconds();
-    let clock = `${hours}:${minutes}:${seconds}`
+    seconds < 10 ? (seconds = `0${seconds}`) : seconds;
+    let clock = `${hours}:${minutes}:${seconds}`;
     let day = time.getDay();
-    let weekdays = ["Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar"]
-    document.getElementById("myClock").innerHTML = clock + " " + weekdays[day-1]
-    console.log(clock);
+    let weekdays = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"];
+
+    document.getElementById("myClock").innerHTML = clock + " " + weekdays[day - 1];
 }
 
 let dummy = setInterval(showTime, 1000);
